db65b397a0507726c9f75bc7bd207d98ba9ef2d1
Adames Guzman, Enmanuel Andres <e.adames11@unapec.edu.do>
2025-06-15 09:04:09 -0400

    Agrega migraciones, modelo producto, y metodo Seed mediante el uso automatico de Bogus

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Parcial.Models;
 
 namespace parcial.Controllers
 {
     public class ProductoController : Controller
     {
+        private readonly AppDbContext _context = new AppDbContext();
         public IActionResult Index()
         {
-            return View();
+            var productos = _context.Producto.ToList();
+            return View(productos);
         }
     }
 }
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,34 @@
+﻿namespace parcial.Migrations
+{
+    using parcial.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Parcial.Models.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Parcial.Models.AppDbContext context)
+        {
+            if (!context.Producto.Any())
+            {
+                var faker = new Bogus.Faker<Models.Producto>()
+                    .RuleFor(x => x.Nombre, f => f.Commerce.ProductName())
+                    .RuleFor(x => x.Precio, f => decimal.Parse(f.Commerce.Price()))
+                    .RuleFor(x => x.Precio, f => f.Random.Int(0, 50));
+
+                var p = faker.Generate(15);
+
+                List<Producto> fakeProductos = faker.Generate(15);
+
+                context.Producto.AddRange(fakeProductos);
+                context.SaveChanges();
+            }
+        }
+    }
+}
